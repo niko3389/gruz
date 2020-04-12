2e2cc1d1ebdb5098b6c1ce886d7e851a5ea8876f
niko <niko33_89@mail.ru>
2020-04-13 01:16:33 +0300

    tele fix

--- telegram.php
+++ telegram.php
@@ -1,38 +1,22 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (!empty($_POST['name']) && !empty($_POST['phone'])){
-    if (isset($_POST['name'])) {
-        if (!empty($_POST['name'])){
-          $uname = strip_tags($_POST['name']);
-          $unameFieldset = "Имя:%20";
-         }
-    }
-    if (isset($_POST['phone'])) {
-        if (!empty($_POST['phone'])){
-          $uphone = strip_tags($_POST['phone']);
-          $uphoneFieldset = "Телефон:%20";
-        }
-    }
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$token = "1166739482:AAGOpC1mmGJaaRGkQI5KYNGCpOLDWTrzKLY";
+$chat_id = "-1001443491648";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone
+);
 
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
 
-    $token = "1166739482:AAGOpC1mmGJaaRGkQI5KYNGCpOLDWTrzKLY";
-    $chat_id = "-481025421";
-    $arr = array(
-      $unameFieldset => $uname,
-      $uphoneFieldset => $uphone
-    );
-    foreach($arr as $key => $value) {
-      $txt .= "<b>".$key."</b>".$value."%0A";
-    };
-    $sendToTelegram = fopen("https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$chat_id."&parse_mode=html&text=".$txt,"r");
-        if ($sendToTelegram == 'false') {
-            echo '<p class="fail"><b>Ошибка. Сообщение не отправлено!</b></p>';
-        } else {
-          echo '<h3 class="success">Спасибо! В ближайшее время наш менеджер свяжется с Вами.</h3>';
-        }
-  } else {
-    echo '<p class="fail">Ошибка. Вы заполнили не все обязательные поля!</p>';
-  }
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
 } else {
-  header ("Location: http://vk.com");
+  echo "Error";
 }
+?>
