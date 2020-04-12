ee9d89eb7dfd9008b5a491b7eed807cebece26ee
niko <niko33_89@mail.ru>
2020-04-12 23:35:33 +0300

    tele dev

--- telegram.php
+++ telegram.php
@@ -1,36 +1,38 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (!empty($_POST['name']) && !empty($_POST['phone'])){
+    if (isset($_POST['name'])) {
+        if (!empty($_POST['name'])){
+          $uname = strip_tags($_POST['name']);
+          $unameFieldset = "Имя:%20";
+         }
+    }
+    if (isset($_POST['phone'])) {
+        if (!empty($_POST['phone'])){
+          $uphone = strip_tags($_POST['phone']);
+          $uphoneFieldset = "Телефон:%20";
+        }
+    }
 
-/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXXXX/getUpdates,
-где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
 
-//Переменная $name,$phone, $mail получает данные при помощи метода POST из формы
-$name = $_POST['user_name'];
-$phone = $_POST['user_phone'];
-
-//в переменную $token нужно вставить токен, который нам прислал @botFather
-$token = "1166739482:AAGOpC1mmGJaaRGkQI5KYNGCpOLDWTrzKLY";
-
-//нужна вставить chat_id (Как получить chad id, читайте ниже)
-$chat_id = "-481025421";
-
-//Далее создаем переменную, в которую помещаем PHP массив
-$arr = array(
-  'Имя пользователя: ' => $name,
-  'Телефон: ' => $phone,
-);
-
-//При помощи цикла перебираем массив и помещаем переменную $txt текст из массива $arr
-foreach($arr as $key => $value) {
-  $txt .= "<b>".$key."</b> ".$value."%0A";
-};
-
-//Осуществляется отправка данных в переменной $sendToTelegram
-$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
-
-//Если сообщение отправлено, напишет "Thank you", если нет - "Error"
-if ($sendToTelegram) {
-  echo "Thank you";
+    $token = "1166739482:AAGOpC1mmGJaaRGkQI5KYNGCpOLDWTrzKLY";
+    $chat_id = "-481025421";
+    $arr = array(
+      $unameFieldset => $uname,
+      $uphoneFieldset => $uphone
+    );
+    foreach($arr as $key => $value) {
+      $txt .= "<b>".$key."</b>".$value."%0A";
+    };
+    $sendToTelegram = fopen("https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$chat_id."&parse_mode=html&text=".$txt,"r");
+        if ($sendToTelegram == 'false') {
+            echo '<p class="fail"><b>Ошибка. Сообщение не отправлено!</b></p>';
+        } else {
+          echo '<h3 class="success">Спасибо! В ближайшее время наш менеджер свяжется с Вами.</h3>';
+        }
+  } else {
+    echo '<p class="fail">Ошибка. Вы заполнили не все обязательные поля!</p>';
+  }
 } else {
-  echo "Error";
+  header ("Location: http://vk.com"); // главная страница вашего лендинга
 }
-?>
